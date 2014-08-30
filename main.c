021aa5572e370808f870c563142b6dadcd003145
menzzana <henric@zazzi.se>
2014-08-30 20:08:26 +0200

    first push

---
+++ main.c
@@ -0,0 +1,44 @@
+/* Parma
+
+Authors: Henric Zazzi, Hannes Leskelä
+Copyright (C) PDC Center for High Performance Computing, KTH 2014  
+
+Licensed under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Copy of the GNU General Public License can be onbtained from
+see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>     
+#include <mpi.h>
+
+const char VERSION[]="Parma v0.1.1";
+
+int main(int argc, char **argv) {
+	int optionvalue;
+	
+	while ((optionvalue=getopt(argc,argv,"f:p:m:"))!=-1)
+		switch (optionvalue) {
+			case 'f':
+				// filename=optarg;
+				break;
+			case 'p':
+				// permutations=optarg
+			case 'm':
+				// number of markers=optarg
+			default:
+				exit(EXIT_FAILURE);
+			}
+  exit(EXIT_SUCCESS);
+  }
